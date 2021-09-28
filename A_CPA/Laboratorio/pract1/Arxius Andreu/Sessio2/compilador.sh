3c2d5e96bb2182308eef75198b6efb541a4b3436
Andreu Mut Portes <amutpor@upv.edu.es>
2021-09-28 17:21:17 +0200

    CPA Lab 1 Sesio 2 finalizada

---
+++ A_CPA/Laboratorio/pract1/Arxius Andreu/Sessio2/compilador.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc -fopenmp -Wall -o bucle1 ~/W/CPA/pract1/sessio2/bucle1.c
+gcc -fopenmp -Wall -o bucle2 ~/W/CPA/pract1/sessio2/bucle2.c
+gcc -fopenmp -Wall -o bucle3 ~/W/CPA/pract1/sessio2/bucle3.c
+gcc -fopenmp -Wall -o bucle4 ~/W/CPA/pract1/sessio2/bucle4.c
---
+++ A_CPA/Laboratorio/pract1/Arxius Andreu/Sessio2/jobBucle2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=5:00
+#SBATCH --partition=cpa
+
+OMP_NUM_THREADS=2 ./bucle2
+OMP_NUM_THREADS=8 ./bucle2
+OMP_NUM_THREADS=32 ./bucle2
